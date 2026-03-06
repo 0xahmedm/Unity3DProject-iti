920a698eb6cdf30b0965f0364b1b91d54c580428
MohammedAljazairy <mohammedaljazairy@icloud.com>
2026-03-06 09:57:44 +0200

    Some improvements and changes to UI and Gameplay

--- Assets/Level 1/L1Scripts/L1MagnetEffect.cs
+++ Assets/Level 1/L1Scripts/L1MagnetEffect.cs
@@ -1,4 +1,6 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class L1MagnetEffect : MonoBehaviour
 {
@@ -6,7 +8,8 @@ public class L1MagnetEffect : MonoBehaviour
 
     public float attractDistance = 20f;
     public float attractSpeed = 25f;
-
+    public RawImage magnet;
+    public Slider slider;
     private float timer;
 
     void Update()
@@ -14,38 +17,37 @@ public class L1MagnetEffect : MonoBehaviour
         if (!magnetActive) return;
 
         timer -= Time.deltaTime;
-
+        magnet.gameObject.SetActive(true);
+        slider.gameObject.SetActive(true);
         AttractCoins();
+        slider.value =Mathf.Lerp(slider.value,timer,50);
 
-        if (timer <= 0)
+        if (timer <= 0){
             magnetActive = false;
+            magnet.gameObject.SetActive(false);
+            slider.gameObject.SetActive(false);
+        }
     }
 
     public void ActivateMagnet(float duration)
     {
         magnetActive = true;
         timer = duration;
+        slider.maxValue=duration;
     }
 
     void AttractCoins()
     {
-        Collider[] coins =
-            Physics.OverlapSphere(
-                transform.position,
-                attractDistance);
+        Collider[] coins = Physics.OverlapSphere(transform.position,attractDistance);
 
         foreach (Collider c in coins)
         {
             if (c.CompareTag("Coin"))
             {
                 Vector3 dir =
-                    (transform.position -
-                    c.transform.position).normalized;
+                    (transform.position -c.transform.position).normalized;
 
-                c.transform.position +=
-                    dir *
-                    attractSpeed *
-                    Time.deltaTime;
+                c.transform.position +=dir *attractSpeed *Time.deltaTime;
             }
         }
     }
--- Assets/Level 1/L1Scripts/L1SpeedBoost.cs
+++ Assets/Level 1/L1Scripts/L1SpeedBoost.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class L1SpeedBoost : MonoBehaviour
 {
@@ -9,14 +10,15 @@ public class L1SpeedBoost : MonoBehaviour
     private float originalSpeed;
 
     private bool active;
+    public RawImage energy;
+    public Slider slider;
 
     void Start()
     {
-        movement =
-            GetComponent<L1PlayerMovement>();
+        movement = GetComponent<L1PlayerMovement>();
 
-        originalSpeed =
-            movement.forwardSpeed;
+        originalSpeed = movement.forwardSpeed;
+
     }
 
     void Update()
@@ -24,25 +26,25 @@ public class L1SpeedBoost : MonoBehaviour
         if (!active) return;
 
         timer -= Time.deltaTime;
+        energy.gameObject.SetActive(true);
+        slider.gameObject.SetActive(true);
+
+        slider.value =Mathf.Lerp(slider.value,timer,10);
 
         if (timer <= 0)
         {
-            movement.forwardSpeed =
-                originalSpeed;
-
+            movement.forwardSpeed =originalSpeed;
+            energy.gameObject.SetActive(false);
+            slider.gameObject.SetActive(false);
             active = false;
         }
     }
 
-    public void ActivateBoost(
-        float duration,
-        float boostAmount)
+    public void ActivateBoost(float duration,float boostAmount)
     {
-        movement.forwardSpeed =
-            originalSpeed + boostAmount;
-
+        movement.forwardSpeed =originalSpeed + boostAmount;
         timer = duration;
-
         active = true;
+        slider.maxValue=duration;
     }
 }
