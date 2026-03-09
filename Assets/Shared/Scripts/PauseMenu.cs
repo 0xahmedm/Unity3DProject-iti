78399ea9be1bf09c86d0f8b0293bfec36f0580cc
MohammedAljazairy <mohammedaljazairy@icloud.com>
2026-03-09 18:22:18 +0200

    The Pause Menu Needs Updating

--- Assets/Shared/Scripts/PauseMenu.cs
+++ Assets/Shared/Scripts/PauseMenu.cs
@@ -1,3 +1,4 @@
+using System.Runtime.Serialization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -18,11 +19,16 @@ public class PauseMenu : MonoBehaviour
     {
         menu.SetActive(!menu.activeSelf);
         Cursor.visible = menu.activeSelf;
+        if(menu.activeSelf)
+            Time.timeScale=0.0f;
+        else
+            Time.timeScale=1.0f;
         Cursor.lockState = menu.activeSelf ? CursorLockMode.None : CursorLockMode.Locked;
     }
 
     public void backToMainMenu()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenue");
     }
     public void QuitGame()
